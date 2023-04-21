8c40c686a96b2dd4e99b7734db0f7d9ec42d1c6a
Jimmy Posadas Esteban <Jimmyhahalol2@gmail.com>
2023-04-21 20:37:40 +0800

    some more updates

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
     <div className="App">
       <Router>
       <NavBar/>
-      
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/Projects" element={<Projects/>}/>
